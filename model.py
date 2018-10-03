c953a83831c4601482071b8125d758024bd81918
Prabhsimran Singh <pskrunner14@gmail.com>
2018-10-04 00:45:11 +0530

    adds mod doc

--- model.py
+++ model.py
@@ -1,3 +1,18 @@
+""" Deep Convolutional Generative Adversarial Network (DCGAN).
+Using deep convolutional generative adversarial networks (DCGAN) to generate
+face images from a noise distribution.
+References:
+    -Generative Adversarial Nets.
+    Goodfellow et al. arXiv: 1406.2661.
+    - Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks. 
+    A Radford, L Metz, S Chintala. arXiv: 1511.06434.
+Links:
+    - [GAN Paper](https://arxiv.org/pdf/1406.2661.pdf)
+    - [DCGAN Paper](https://arxiv.org/abs/1511.06434).
+Author: Prabhsimran Singh
+Project: https://github.com/pskrunner14/faceGAN
+"""
+
 import tensorflow as tf
 
 class FaceGAN():
